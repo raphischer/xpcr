762c6f78a2cad4cef43592e81d3f631cf4018e95
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2023-03-17 16:29:31 +0100

    improved meta learning

--- meta_learn.py
+++ meta_learn.py
@@ -1,17 +1,34 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+# preprocessing, metrics & co
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, LabelEncoder
+from sklearn.model_selection import cross_validate
+from sklearn.utils import resample
+from sklearn.model_selection import KFold, StratifiedKFold, GroupKFold
+from sklearn.metrics import check_scoring
+
+# ML methods
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import RidgeClassifier
 from sklearn.svm import SVC
 from sklearn.dummy import DummyClassifier
-from sklearn.linear_model import SGDClassifier, LogisticRegression
-from sklearn.utils import resample
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.model_selection import KFold, StratifiedKFold
-from sklearn.model_selection import cross_validate
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+
+# suppress warnings
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
 
 
 def prepare_features(df, features):
@@ -64,49 +81,100 @@ def print_cv_scoring_results(model_name, scoring, scores):
     print(f'{model_name:<20}' + ' - '.join([f'{metric:<10} {mean_v} +- {std_v}' for metric, (mean_v, std_v) in results.items()]))
 
 
-features = {
-    'seasonality':              OneHotEncoder(),
-    'freq':                     OneHotEncoder(),
-    'forecast_horizon':         OneHotEncoder(),
-    'contain_missing_values':   OneHotEncoder(),
-    'contain_equal_length':     OneHotEncoder(),
-    'model':                    OneHotEncoder()
-}
-
-classifiers = {
-    'Random': DummyClassifier(strategy='uniform'),
-    'Linear': LogisticRegression(penalty='none'),
-    'Ridge': RidgeClassifier(),
-    'Lasso': LogisticRegression(penalty='l1', solver='liblinear'),
-    # 'SGD': SGDClassifier(),
-    'SVM': SVC(),
-    'AdaBoost': AdaBoostClassifier(),
-    'Random Forest': RandomForestClassifier(),
-    'MLP': MLPClassifier(max_iter=500, early_stopping=True)
-}
-
-database = pd.read_pickle('meta_learn.pkl')
-
-for feat in features.keys():
-    print(f'{feat:<30} {pd.unique(database[feat])}')
-
-X, fnames, fitted_transform = prepare_features(database, features)
-y = database['compound'].values
-print(np.unique(y, return_counts=True))
-y[y <= 1] = 1
-y[y > 1] = 0
-print(np.unique(y, return_counts=True))
-
-scoring = ['accuracy', 'balanced_accuracy']
-cv = StratifiedKFold(n_splits=5)
-
-for cls_name, cls_ in classifiers.items():
-    scores = cross_validate(cls_, X, y, scoring=scoring, cv=cv, return_train_score=True)
-    print_cv_scoring_results(cls_name, scoring, scores)
-
 # def retransform_encoding(X, fitted_transform, fnames):
 #     X_retransformed = {}
 #     for transform_name, transform in fitted_transform.items():
 #         f_idc = np.array([idx for idx, fname in enumerate(fnames) if transform_name in fname], dtype=int)
 #         X_retransformed[transform_name] = transform.inverse_transform(X[:, f_idc])
 #     return X_retransformed
+
+
+FEATURES = {
+    'freq':                     OneHotEncoder(),
+    'forecast_horizon':         OneHotEncoder(),
+    'contain_missing_values':   OneHotEncoder(),
+    'contain_equal_length':     OneHotEncoder(),
+    'model':                    OneHotEncoder(),
+    'num_ts':                   None,
+    'avg_ts_len':               None,
+    'avg_ts_mean':              None,
+    'avg_ts_min':               None,
+    'avg_ts_max':               None
+}
+
+
+CLASSIFIERS = {
+    "Random":               DummyClassifier(strategy='uniform'),
+    # "Ridge":                RidgeClassifier(),
+    "Nearest Neighbors":    KNeighborsClassifier(3),
+    # "Linear SVM":           SVC(kernel="linear", C=0.025),
+    # "RBF SVM":              SVC(gamma=2, C=1),
+    "Gaussian Process":     GaussianProcessClassifier(1.0 * RBF(1.0)),
+    "Decision Tree":        DecisionTreeClassifier(max_depth=5),
+    "Random Forest":        RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    "Neural Net":           MLPClassifier(alpha=1, max_iter=1000),
+    "AdaBoost":             AdaBoostClassifier(),
+    "Naive Bayes":          GaussianNB(),
+    "QDA":                  QuadraticDiscriminantAnalysis(),
+}
+
+
+# meta learn arguments
+N_SPLITS =          5
+TARGET_COLUMNS =    ['compound', 'running_time', 'parameters', 'RMSE']
+SCORING =           ['accuracy', 'f1']
+CV =                GroupKFold(n_splits=N_SPLITS) # KFold StratifiedKFold
+GROUP_BY =          'dataset' # 'model'
+METRIC_FIELD =      'index'
+
+
+def split_y(y_train, y_test=None, quantile=None):
+    quant = np.quantile(y_train, [0.5])
+    y_train = [0 if y_ < quant else 1 for y_ in y_train]
+    if y_test is not None:
+        y_test = [0 if y_ < quant else 1 for y_ in y_test]
+    return y_train, y_test
+
+
+def run_meta_learn(database, only_print_better_than_random=False):
+
+    # load features and prepare ML pipeline
+    X, fnames, fitted_transform = prepare_features(database, FEATURES)
+    group_info = LabelEncoder().fit_transform(database[GROUP_BY].values)
+    print(f'\n\n:::::::::::::::::::::::::::: {"RUNNING ON " + str(X.shape) + " FEATURES":^35} ::::::::::::::::::::::::::::\n\n')
+
+    for col in TARGET_COLUMNS:
+        
+        # make binary class problem
+        y = np.array([val[METRIC_FIELD] if isinstance(val, dict) else val for val in database[col]])
+        print(f'\n\n:::::::::::::::::::::::::::: {"Prediction of " + col:^35} ::::::::::::::::::::::::::::')
+        # labels, counts = np.unique(y, return_counts=True)
+        # label_header = f"{counts[0]}x{labels[0]} and {counts[1]}x{labels[1]}, split at {quant[0]:5.3f}"
+        # print(f':::::::::::::::::::::::::::: {label_header:^35} ::::::::::::::::::::::::::::\n')
+
+        best_name, best_acc, best_scores = '', 0, None
+        for cls_name, cls_ in CLASSIFIERS.items():
+            # scores = cross_validate(cls_, X, y, scoring=SCORING, cv=CV, groups=group_info, return_train_score=True)
+            scores = {}
+            # custom CV to ensure splitting y labels only on training y
+            for idx, (train_idx, test_idx) in enumerate(CV.split(X, y, group_info)):
+                X_train, X_test, y_train, y_test = X[train_idx], X[test_idx], y[train_idx], y[test_idx]
+                y_train, y_test = split_y(y_train, y_test)
+                cls_.fit(X_train, y_train)
+                for score in SCORING:
+                    scorer = check_scoring(cls_, score)
+                    for split, X_pred, y_true in zip(['train', 'test'], [X_train, X_test], [y_train, y_test]):
+                        score_name = f'{split}_{score}'
+                        if score_name not in scores:
+                            scores[score_name] = []
+                        scores[score_name].append(scorer(cls_, X_pred, y_true))
+            
+            # print scoring and best method
+            if not only_print_better_than_random or np.mean(scores['test_f1']) > 0.5:
+                print_cv_scoring_results(cls_name, SCORING, scores)
+            if np.mean(scores['test_f1']) > np.mean(best_acc):
+                best_name = cls_name
+                best_acc = scores['test_f1']
+                best_scores = scores
+        print('----------- BEST METHOD:')
+        print_cv_scoring_results(best_name, SCORING, best_scores)
--- run_evaluation.py
+++ run_evaluation.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     parser.add_argument("--database-fname", default="results/database.pkl", help="filename for the database that shall be created")
     parser.add_argument("--boundaries", default="boundaries.json")
     parser.add_argument("--clean", action="store_true", help="set to first delete all content in given output directories")
-    parser.add_argument("--mode", default='meta', choices=['meta', 'interactive', 'paper_results', 'label', 'stats'])
+    parser.add_argument("--mode", default='stats', choices=['meta', 'interactive', 'paper_results', 'label', 'stats'])
     # interactive exploration
     parser.add_argument("--host", default='localhost', type=str, help="default host") # '0.0.0.0'
     parser.add_argument("--port", default=8888, type=int, help="default port")
@@ -32,80 +32,110 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    meta = load_meta()
+    meta_learn_data_fname = 'meta_learn.pkl'
 
-    if os.path.isfile(args.database_fname): # if available, read from disc
-        database = pd.read_pickle(args.database_fname)
+    # load meta learn data if already processed!
+    if args.mode == 'meta' and os.path.isfile(meta_learn_data_fname):
+            meta_learn_data = pd.read_pickle(meta_learn_data_fname)
+
     else:
-        database = load_database(args.logdir_root, args.output_logdir_merged, None, args.property_extractors_module, args.clean)
-        database.to_pickle(args.database_fname)
 
-    rated_database, boundaries, real_boundaries, _ = rate_database(database, properties_meta=meta['properties'], boundaries=args.boundaries)
+        meta = load_meta()
+        if os.path.isfile(args.database_fname): # if available, read from disc
+            database = pd.read_pickle(args.database_fname)
+        else:
+            database = load_database(args.logdir_root, args.output_logdir_merged, None, args.property_extractors_module, args.clean)
+            database.to_pickle(args.database_fname)
 
-    print(f'Database constructed from logs has {rated_database.shape} entries')
+        rated_database, boundaries, real_boundaries, _ = rate_database(database, properties_meta=meta['properties'], boundaries=args.boundaries)
+        print(f'Database constructed from logs has {rated_database.shape} entries')
 
-    if args.mode == 'interactive':
-        app = Visualization(rated_database, boundaries, real_boundaries, meta)
-        app.run_server()#debug=args.debug, host=args.host, port=args.port)
+        if args.mode == 'interactive':
+            app = Visualization(rated_database, boundaries, real_boundaries, meta)
+            app.run_server()#debug=args.debug, host=args.host, port=args.port)
 
-    if args.mode == 'label':
-        summary = fill_meta(rated_database.iloc[0].to_dict(), meta)
-        pdf_doc = PropertyLabel(summary, 'optimistic median')
-        pdf_doc.save('label.pdf')
+        if args.mode == 'label':
+            summary = fill_meta(rated_database.iloc[0].to_dict(), meta)
+            pdf_doc = PropertyLabel(summary, 'optimistic median')
+            pdf_doc.save('label.pdf')
 
-    if args.mode == 'stats':
-        grouped_by = rated_database.groupby(['dataset', 'task'])
-        for idx, ((ds, task), data) in enumerate(iter(grouped_by)):
-            for col in data.columns:
-                if data[col].dropna().size == 0:
-                    data = data.drop(col, axis=1)
-            runtime_field = "running_time" if task == "infer" else "train_running_time"
-            if runtime_field in data.columns:
-                time = sum([val['value'] for val in data[runtime_field] if isinstance(val, dict)])
-                time_n_nan = sum([1 for val in data[runtime_field] if not isinstance(val, dict)])
-            else:
-                time, time_n_nan = -1, -1
-            print(f'{idx:<2} {task:<5} {ds:<45} {str(data.shape):<8} entries, processing time {time / 3600:6.2f} h ({time_n_nan} missing time infos)')
+        if args.mode == 'stats':
+            grouped_by = rated_database.groupby(['dataset'])
+            ds_stats = []
+            max_shape = [0, 0]
+            for (ds), data in iter(grouped_by):
+                stats = {'ds': ds}
+                for col in data.columns:
+                    if data[col].dropna().size == 0:
+                        data = data.drop(col, axis=1)
+                stats['entries'] = data.shape
+                if data.shape[0] > max_shape[0] or data.shape[1] > max_shape[1]:
+                    max_shape = [data.shape[0], data.shape[1]]
+                if "running_time" in data:
+                    stats['time_infer'] = sum([val['value'] for val in data["running_time"] if isinstance(val, dict)])
+                else:
+                    stats['time_infer'] = np.inf
+                if "train_running_time" in data:
+                    stats['time_train'] = sum([val['value'] for val in data["train_running_time"] if isinstance(val, dict)]) / 2 * 40 # TODO remove the epoch hack
+                else:
+                    stats['time_train'] = np.inf
+                stats['time_total'] = stats['time_train'] + stats['time_infer']
+                ds_stats.append(stats)
+            print(max_shape)
+            sorted_ds_stats = sorted(ds_stats, key=lambda d: d['time_total'])
+            to_rerun = []
+            for idx, ds_stat in enumerate(sorted_ds_stats):
+                if ds_stat["entries"][0] >= max_shape[0] - 1 and ds_stat["entries"][1] == max_shape[1]: # TODO remove -1
+                    to_rerun.append(ds_stat["ds"])
+                    print(f'SUCCESS ON {idx:<2} {ds_stat["ds"]:<45} {str(ds_stat["entries"]):<8} entries, time total {ds_stat["time_total"] / 60:6.2f} m (train {ds_stat["time_train"] / 60:6.2f} m, infer {ds_stat["time_infer"] / 60:6.2f} m)')
+                # else:
+                #     print(f'ERRORS ON  {idx:<2} {ds_stat["ds"]:<45} {str(ds_stat["entries"]):<8} entries, time total {ds_stat["time_total"] / 3600:6.2f} h (train {ds_stat["time_train"] / 3600:6.2f} h, infer {ds_stat["time_infer"] / 3600:6.2f} h)')
+            print('"' + '" "'.join(to_rerun) + '"')
 
+        if args.mode == 'paper_results':
+            pass
+        
+        if args.mode == 'meta':
+            # only look at infer results
+            rated_database = rated_database.drop(rated_database[rated_database['task'] != 'infer'].index)
+            grouped_by = rated_database.groupby(['dataset'])
+            # check for completeness of results
+            max_shape = (0, 0)
+            for idx, ((ds), data) in enumerate(iter(grouped_by)):
+                shape = data.shape
+                if shape[0] > max_shape[0] or shape[1] > max_shape[1]:
+                    max_shape = shape
+            for idx, ((ds), data) in enumerate(iter(grouped_by)):
+                if data.shape != max_shape:
+                    # discard these results due to incompleteness
+                    mod_missing = " ".join([mod for mod in pd.unique(rated_database["model"]) if mod not in pd.unique(data['model'])])
+                    print(f'Dropping {ds:<40} - shape {str(data.shape):<8} does not match expected {str(max_shape):<8} - missing models: {mod_missing}')
+                    rated_database = rated_database.drop(data.index)
+                else:
+                    # store dataset specific meta features
+                    full_path = os.path.join('mnt_data/data', ds + '.tsf')
+                    ts_data, freq, seasonality, forecast_horizon, contain_missing_values, contain_equal_length = load_data(full_path)
+                    rated_database.loc[data.index,'num_ts'] = ts_data.shape[0]
+                    rated_database.loc[data.index,'avg_ts_len'] = ts_data['series_value'].map(lambda ts: len(ts)).mean()
+                    rated_database.loc[data.index,'avg_ts_mean'] = ts_data['series_value'].map(lambda ts: np.mean(ts)).mean()
+                    rated_database.loc[data.index,'avg_ts_min'] = ts_data['series_value'].map(lambda ts: np.min(ts)).mean()
+                    rated_database.loc[data.index,'avg_ts_max'] = ts_data['series_value'].map(lambda ts: np.max(ts)).mean()
+                    rated_database.loc[data.index,'seasonality'] = seasonality
+                    rated_database.loc[data.index,'freq'] = freq
+                    rated_database.loc[data.index,'forecast_horizon'] = forecast_horizon
+                    rated_database.loc[data.index,'contain_missing_values'] = contain_missing_values
+                    rated_database.loc[data.index,'contain_equal_length'] = contain_equal_length
+            rated_database.to_pickle(meta_learn_data_fname)
+            meta_learn_data = rated_database
+        
     if args.mode == 'meta':
-        # only look at infer results
-        rated_database = rated_database.drop(rated_database[rated_database['task'] != 'infer'].index)
-        grouped_by = rated_database.groupby(['dataset'])
-        # check for completeness of results
-        max_shape = (0, 0)
-        for idx, ((ds), data) in enumerate(iter(grouped_by)):
-            shape = data.shape
-            if shape[0] > max_shape[0] or shape[1] > max_shape[1]:
-                max_shape = shape
-        for idx, ((ds), data) in enumerate(iter(grouped_by)):
-            if data.shape != max_shape:
-                print(f'Dropping {ds} - shape {data.shape} does not match expected {max_shape}')
-                rated_database = rated_database.drop(data.index)
-            else:
-                full_path = os.path.join('mnt_data/data', ds + '.tsf')
-                ts_data, freq, seasonality, forecast_horizon, contain_missing_values, contain_equal_length = load_data(full_path)
-                rated_database.loc[data.index,'num_ts'] = ts_data.shape[0]
-                rated_database.loc[data.index,'avg_ts_len'] = ts_data['series_value'].map(lambda ts: len(ts)).mean()
-                rated_database.loc[data.index,'avg_ts_val'] = ts_data['series_value'].map(lambda ts: np.mean(ts)).mean()
-                rated_database.loc[data.index,'avg_ts_min'] = ts_data['series_value'].map(lambda ts: np.min(ts)).mean()
-                rated_database.loc[data.index,'avg_ts_max'] = ts_data['series_value'].map(lambda ts: np.max(ts)).mean()
-                rated_database.loc[data.index,'seasonality'] = seasonality
-                rated_database.loc[data.index,'freq'] = freq
-                rated_database.loc[data.index,'forecast_horizon'] = forecast_horizon
-                rated_database.loc[data.index,'contain_missing_values'] = contain_missing_values
-                rated_database.loc[data.index,'contain_equal_length'] = contain_equal_length
-        rated_database.to_pickle('meta_learn.pkl')
-        shape = rated_database.shape
-        ds = pd.unique(rated_database["dataset"])
-        models = pd.unique(rated_database["model"])
+
+        from meta_learn import run_meta_learn
+
+        models = pd.unique(meta_learn_data["model"])
+        shape = meta_learn_data.shape
+        ds = pd.unique(meta_learn_data["dataset"])
         print(f'Meta learning to be run on {shape} database entries, with a total of {len(ds)} datasets and {len(models)} models!')
-
-
-    if args.mode == 'paper_results':
-        pass
-
-    # for task, task_agg_logs in aggregated_logs.items():
-    #     print('Results for', task)
-    #     total_ds = pd.unique(task_agg_logs['dataset']).size
-    #     best_per_ds = task_agg_logs.groupby('dataset').apply( lambda grp: grp.iloc[grp['RMSE'].argmin()].drop('dataset') )
-    #     print(best_per_ds)
+        print('Available datasets:', ' '.join(ds))
+        
+        run_meta_learn(meta_learn_data)
