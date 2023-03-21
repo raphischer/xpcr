e68cd45b325cf71b8182cc2ca9ff934d4404c17a
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2023-03-21 16:32:57 +0100

    updated run_all script

--- run_all.sh
+++ run_all.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-for d in "m1_quarterly_dataset" "cif_2016_dataset" "solar_weekly_dataset" "tourism_quarterly_dataset" "fred_md_dataset" "nn5_daily_dataset_without_missing_values" "electricity_weekly_dataset" "nn5_weekly_dataset" "hospital_dataset" "m3_quarterly_dataset" "m1_monthly_dataset" "traffic_weekly_dataset" "m4_weekly_dataset" "tourism_monthly_dataset" "bitcoin_dataset_without_missing_values" "us_births_dataset" "saugeenday_dataset" "covid_deaths_dataset" "vehicle_trips_dataset_without_missing_values" "sunspot_dataset_without_missing_values" "pedestrian_counts_dataset" "weather_dataset"
+# "bitcoin_dataset_without_missing_values" fails for ds-seed 42 on deepfactor deeprenewal deepstate mqcnn mqrnn and nbeats???!!!
+# TODO full command
+# for d in "m1_quarterly_dataset" "solar_weekly_dataset" "tourism_quarterly_dataset" "fred_md_dataset" "nn5_daily_dataset_without_missing_values" "electricity_weekly_dataset" "nn5_weekly_dataset" "hospital_dataset" "m3_quarterly_dataset" "m1_monthly_dataset" "traffic_weekly_dataset" "m4_weekly_dataset" "tourism_monthly_dataset" "covid_deaths_dataset" "cif_2016_dataset" "pedestrian_counts_dataset" "vehicle_trips_dataset_without_missing_values"
+# TODO ws05
+# -1 seed for "m1_quarterly_dataset" "solar_weekly_dataset" "tourism_quarterly_dataset" "fred_md_dataset" "nn5_daily_dataset_without_missing_values" "electricity_weekly_dataset" "nn5_weekly_dataset" "hospital_dataset" "m3_quarterly_dataset" "m1_monthly_dataset"
+# TODO ws022
+# ------------
+for d in "traffic_weekly_dataset" "m4_weekly_dataset" "tourism_monthly_dataset" "covid_deaths_dataset" "cif_2016_dataset" "pedestrian_counts_dataset" "vehicle_trips_dataset_without_missing_values"
 do
     for m in "deepar" "deepstate" "deepfactor" "deeprenewal" "gpforecaster" "mqcnn" "mqrnn" "nbeats" "tempfus" "wavenet" "feedforward"
     do
-        for s in "42" "135" "468" "129" "124"
+        for s in "42" "135" "468" "129" "124" "-1"
         do
-            python run.py --dataset $d --model $m --ds-seed $s
+        python run.py --dataset $d --model $m --ds-seed $s --output-dir $1
         done
     done
 done
-# TODO fix parameter error for "naiveseasonal" "rotbaum"
+# TODO if there is enough time, also run on weather?
+# TODO fix parameter assessment error for "naiveseasonal" "rotbaum"
 # TODO fix error for "transformer"
 # TODO also run "arima" (takes very long)
+# TODO only single series -> no subsampling possible! "us_births_dataset" "saugeenday_dataset" "sunspot_dataset_without_missing_values"
 # TODO investigate errors for "m4_monthly_dataset" "traffic_hourly_dataset" "electricity_hourly_dataset" "solar_10_minutes_dataset" "kdd_cup_2018_dataset_without_missing_values" "kaggle_web_traffic_weekly_dataset" "m4_quarterly_dataset" "cif_2016_dataset" "nn5_daily_dataset_without_missing_values" "tourism_yearly_dataset" "tourism_quarterly_dataset" "tourism_monthly_dataset" "m1_yearly_dataset" "m1_quarterly_dataset" "m1_monthly_dataset" "m3_yearly_dataset" "m3_quarterly_dataset" "m3_monthly_dataset" "m3_other_dataset" "m4_weekly_dataset" "m4_daily_dataset" "m4_hourly_dataset" "car_parts_dataset_without_missing_values" "hospital_dataset" "fred_md_dataset" "nn5_weekly_dataset" "traffic_weekly_dataset" "electricity_weekly_dataset" "solar_weekly_dataset" "dominick_dataset" "us_births_dataset" "saugeenday_dataset" "sunspot_dataset_without_missing_values" "covid_deaths_dataset" "weather_dataset" "pedestrian_counts_dataset" "bitcoin_dataset_without_missing_values" "vehicle_trips_dataset_without_missing_values" "australian_electricity_demand_dataset" "rideshare_dataset_without_missing_values" "temperature_rain_dataset_without_missing_values"
