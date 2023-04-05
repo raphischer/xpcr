d0670b3e7f9d58a9bec21021c39b14db390f1b53
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2023-04-05 11:06:09 +0200

    updated log parsing

--- parse_logs.py
+++ parse_logs.py
@@ -2,7 +2,7 @@ from mlprops.load_experiment_logs import load_database
 
 RESULT_PATHS = [
     ('mnt_data/results', 'results/merged22', 'results/database22.pkl')
-    ('mnt_data/results05', 'results/merged05', 'results/database05.pkl')
+    # ('mnt_data/results05', 'results/merged05', 'results/database05.pkl')
 ]
 
 for logdir, mergedir, database_name in RESULT_PATHS:
