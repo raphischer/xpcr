dc85e221e5a678ba5d24003856fe454d34f0633d
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2023-09-28 16:44:16 +0200

    updated scripts

--- parse_logs.py
+++ parse_logs.py
@@ -1,9 +1,8 @@
 from mlprops.load_experiment_logs import load_database
 
 RESULT_PATHS = [
-    # ('mnt_data/results', 'results/merged22', 'results/database22.pkl')
-    ('mnt_data/autokeras', 'results/merged_autokeras', 'results/autokeras.pkl')
-    # ('mnt_data/results05', 'results/merged05', 'results/database05.pkl')
+    ('mnt_data/autokeras_new', 'results/merged_autokeras_new', 'results/autokeras.pkl')
+    ('mnt_data/results_new03', 'results/merged_new03', 'results/new03.pkl')
 ]
 
 for logdir, mergedir, database_name in RESULT_PATHS:
--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# for d in "m1_quarterly_dataset" "solar_weekly_dataset" "tourism_quarterly_dataset" "fred_md_dataset" "nn5_daily_dataset_without_missing_values" "electricity_weekly_dataset" "nn5_weekly_dataset" "hospital_dataset" "m3_quarterly_dataset" "m1_monthly_dataset" "traffic_weekly_dataset" "m4_weekly_dataset" "tourism_monthly_dataset" "covid_deaths_dataset" "cif_2016_dataset" "pedestrian_counts_dataset" "vehicle_trips_dataset_without_missing_values"
-
-for d in "m1_yearly_dataset" "cif_2016_dataset" "solar_weekly_dataset" "m1_quarterly_dataset" "nn5_weekly_dataset" "tourism_yearly_dataset" "m3_other_dataset" "m3_yearly_dataset" "m3_quarterly_dataset" "tourism_quarterly_dataset" "electricity_weekly_dataset" "car_parts_dataset_without_missing_values" "m1_monthly_dataset" "hospital_dataset" "fred_md_dataset" "traffic_weekly_dataset" "nn5_daily_dataset_without_missing_values" "tourism_monthly_dataset" "m3_monthly_dataset" "m4_weekly_dataset"
+for d in "cif_2016_dataset" "m1_quarterly_dataset" "fred_md_dataset" "tourism_quarterly_dataset" "solar_weekly_dataset" "electricity_weekly_dataset" "nn5_daily_dataset_without_missing_values" "tourism_monthly_dataset" "m1_monthly_dataset" "nn5_weekly_dataset" "hospital_dataset" "m3_quarterly_dataset" "car_parts_dataset_without_missing_values" "m4_weekly_dataset" "traffic_weekly_dataset" "m3_monthly_dataset" "m4_hourly_dataset" "dominick_dataset" "australian_electricity_demand_dataset"
 do
     for m in "deepar" "deepstate" "deepfactor" "deeprenewal" "gpforecaster" "mqcnn" "mqrnn" "nbeats" "tempfus" "wavenet" "feedforward"
     do
@@ -11,10 +9,12 @@ do
         python run.py --dataset $d --model $m --ds-seed $s --output-dir $1
         done
     done
+
+    python run.py --dataset $d --model autokeras --ds-seed -1 --output-dir $2
 done
-# TODO if there is enough time, also run on weather?
+# TODO errors for "m1_yearly_dataset" "tourism_yearly_dataset" "m3_other_dataset" "m3_yearly_dataset"
+# TODO if there is enough time, also run on ""m4_quarterly_dataset"?
 # TODO fix parameter assessment error for "naiveseasonal" "rotbaum"
 # TODO fix error for "transformer"
 # TODO also run "arima" (takes very long)
 # TODO only single series -> no subsampling possible! "us_births_dataset" "saugeenday_dataset" "sunspot_dataset_without_missing_values"
-# TODO investigate errors for "m4_monthly_dataset" "traffic_hourly_dataset" "electricity_hourly_dataset" "solar_10_minutes_dataset" "kdd_cup_2018_dataset_without_missing_values" "kaggle_web_traffic_weekly_dataset" "m4_quarterly_dataset" "cif_2016_dataset" "nn5_daily_dataset_without_missing_values" "tourism_yearly_dataset" "tourism_quarterly_dataset" "tourism_monthly_dataset" "m1_yearly_dataset" "m1_quarterly_dataset" "m1_monthly_dataset" "m3_yearly_dataset" "m3_quarterly_dataset" "m3_monthly_dataset" "m3_other_dataset" "m4_weekly_dataset" "m4_daily_dataset" "m4_hourly_dataset" "car_parts_dataset_without_missing_values" "hospital_dataset" "fred_md_dataset" "nn5_weekly_dataset" "traffic_weekly_dataset" "electricity_weekly_dataset" "solar_weekly_dataset" "dominick_dataset" "us_births_dataset" "saugeenday_dataset" "sunspot_dataset_without_missing_values" "covid_deaths_dataset" "weather_dataset" "pedestrian_counts_dataset" "bitcoin_dataset_without_missing_values" "vehicle_trips_dataset_without_missing_values" "australian_electricity_demand_dataset" "rideshare_dataset_without_missing_values" "temperature_rain_dataset_without_missing_values"
